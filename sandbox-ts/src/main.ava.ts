81881c2d4ff70beec1bea737c356e75b4d73e872
nathan <thenathannicholson@gmail.com>
2024-02-25 20:03:36 -0800

    start integration test

--- sandbox-ts/src/main.ava.ts
+++ sandbox-ts/src/main.ava.ts
@@ -1,44 +1,46 @@
-import { Worker, NearAccount } from 'near-workspaces';
-import anyTest, { TestFn } from 'ava';
+import { Worker, NearAccount } from "near-workspaces"
+import anyTest, { TestFn } from "ava"
 
 const test = anyTest as TestFn<{
-  worker: Worker;
-  accounts: Record<string, NearAccount>;
-}>;
+  worker: Worker
+  accounts: Record<string, NearAccount>
+}>
 
 test.beforeEach(async (t) => {
   // Init the worker and start a Sandbox server
-  const worker = await Worker.init();
+  const worker = await Worker.init()
 
   // Deploy contract
-  const root = worker.rootAccount;
-  const contract = await root.createSubAccount('test-account');
+  const root = worker.rootAccount
+  const contract = await root.createSubAccount("test-account")
   // Get wasm file path from package.json test script in folder above
-  await contract.deploy(
-    process.argv[2],
-  );
+  await contract.deploy(process.argv[2])
 
   // Save state for test runs, it is unique for each test
-  t.context.worker = worker;
-  t.context.accounts = { root, contract };
-});
+  t.context.worker = worker
+  t.context.accounts = { root, contract }
+})
 
 test.afterEach.always(async (t) => {
   // Stop Sandbox server
   await t.context.worker.tearDown().catch((error) => {
-    console.log('Failed to stop the Sandbox:', error);
-  });
-});
+    console.log("Failed to stop the Sandbox:", error)
+  })
+})
 
-test('returns the default greeting', async (t) => {
-  const { contract } = t.context.accounts;
-  const greeting: string = await contract.view('get_greeting', {});
-  t.is(greeting, 'Hello');
-});
+test("creates a new post", async (t) => {
+  const { root, contract } = t.context.accounts
 
-test('changes the greeting', async (t) => {
-  const { root, contract } = t.context.accounts;
-  await root.call(contract, 'set_greeting', { greeting: 'Howdy' });
-  const greeting: string = await contract.view('get_greeting', {});
-  t.is(greeting, 'Howdy');
-});
+  const post: {
+    title: string
+    tags: string[]
+  } = await root.call(contract, "add_post", {
+    title: "Test",
+    description: "Test Description",
+    tags: "tag1,tag2,tag3",
+    media: "post.png",
+  })
+
+  t.is(post.title, "Test")
+  t.is(post.tags[2], "tag3")
+})
